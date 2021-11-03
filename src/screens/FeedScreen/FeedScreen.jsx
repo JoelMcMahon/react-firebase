71f7456b38bf4865a6d5228cf7b07c0a30c2d2e5
JoelM <joelmcmahon@Joels-MacBook-Pro-2.local>
2021-11-03 11:47:53 +0000

    add getPosts function

--- src/screens/FeedScreen/FeedScreen.jsx
+++ src/screens/FeedScreen/FeedScreen.jsx
@@ -1,27 +1,31 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SectionList } from "react-native";
 import { db } from "../../firebase/config";
 
 const FeedScreen = () => {
-  const pullData = async () => {
-    // await db.collection("allPosts").onSnapshot((snapshot) => {
-    //   console.log(snapshot);
-    // });
+  const [posts, setPosts] = useState([]);
 
-    await db
-      .collection("allPosts")
+  useEffect(() => {
+      const getPosts = async () => { await db.collection("allPosts").onSnapshot((snapshot) => {
+        let allPosts = [];
+        snapshot.forEach((doc) => {
+          allPosts.unshift(doc.data());
+        });
+        setPosts(allPosts);}
+       
+    
+    , []);
+  });
 
-      .then((doc) => {
-        console.log(doc.exists);
-      });
-    // console.log(Object.keys(doc));
-    // const data = await doc.data();
-    // console.log(data);
-  };
+  
 
   return (
     <View>
-      <Text onPress={pullData}>Hey Hey</Text>
+      {/* <Text onPress={pullData}>Hey Hey</Text> */}
+      <SectionList
+        sections={posts}
+        renderItem={(post) => <Text>{post.caption}</Text>}
+      />
     </View>
   );
 };
